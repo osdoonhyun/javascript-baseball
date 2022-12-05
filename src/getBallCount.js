f49124712ac429e0b386e06984b1f4b58da6bc68
osdoonhyun <skw1996@naver.com>
2022-12-05 23:38:09 +0900

    feat[getBallCount]: 입력 값과 컴퓨터의 수 비교해서 볼 갯수 세는 기능 구현

---
+++ src/getBallCount.js
@@ -0,0 +1,9 @@
+const getBallCount = (answer, input) => {
+  let ballCount = 0;
+  answer.split('').forEach((value, index) => {
+    if (input.includes(value) && value !== input(index)) ballCount += 1;
+  });
+  return ballCount;
+};
+
+module.exports = getBallCount;
