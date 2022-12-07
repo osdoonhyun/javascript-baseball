76562c36bade2344fe694efe010b5ff9867069a4
osdoonhyun <skw1996@naver.com>
2022-12-07 21:28:31 +0900

    fix(getBallCount): 오타 수정

--- src/getBallCount.js
+++ src/getBallCount.js
@@ -1,7 +1,7 @@
 const getBallCount = (answer, input) => {
   let ballCount = 0;
   answer.split('').forEach((value, index) => {
-    if (input.includes(value) && value !== input(index)) ballCount += 1;
+    if (input.includes(value) && value !== input[index]) ballCount += 1;
   });
   return ballCount;
 };
