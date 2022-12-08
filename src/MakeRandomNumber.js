b49c99b00880c420eea4a0904994fc80689e0266
osdoonhyun <skw1996@naver.com>
2022-12-08 17:16:55 +0900

    fix(MakeRandomNumber): 랜덤 숫자 배열 -> 문자열로 수정

--- src/MakeRandomNumber.js
+++ src/MakeRandomNumber.js
@@ -1,7 +1,11 @@
 const { Random } = require('@woowacourse/mission-utils');
 
 const MakeRandomNumber = () => {
-  return Random.pickUniqueNumbersInRange(1, 9, 3);
+  const set = new Set();
+  while (set.size < 3) {
+    set.add(Random.pickNumberInRange(1, 9));
+  }
+  return [...set].join('');
 };
 
 module.exports = MakeRandomNumber;
