61faf7d2dce4e2acf00f97c6ed1e1de7038bfb1b
osdoonhyun <skw1996@naver.com>
2022-12-08 16:52:20 +0900

    feat(App): 숫자야구 게임 동작 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
+const { Console } = require('@woowacourse/mission-utils');
+const OutputView = require('./OutputView');
+const InputView = require('./InputView');
+const BaseballGame = require('./BaseballGame');
+const MakeRandomNumber = require('./MakeRandomNumber');
+const getStrikeCount = require('./GetStrikeCount');
+const getBallCount = require('./GetBallCount');
+const Validation = require('./Validation');
+
 class App {
-  play() {}
+  #myGame;
+
+  play() {
+    OutputView.gameStart();
+    return this.gameSetting();
+  }
+
+  gameSetting() {
+    this.#myGame = new BaseballGame(MakeRandomNumber());
+    return this.inputMyAnswer();
+  }
+
+  inputMyAnswer() {
+    InputView.inputUserAnswer((userInput) => {
+      if (Validation.validateUserInput(userInput))
+        return this.getResult(userInput);
+
+      return this.throwError();
+    });
+  }
+
+  getResult(myInput) {
+    const { strike, ball } = this.#myGame.getResult(
+      myInput,
+      getStrikeCount,
+      getBallCount
+    );
+    OutputView.showGameResult(strike, ball);
+    if (this.#myGame.isFinishedGame(myInput)) return this.retryCheck();
+    return this.inputMyAnswer();
+  }
+
+  retryCheck() {
+    OutputView.endGame();
+    InputView.inputRetryOrQuit((input) => {
+      if (input === '1') return this.gameSetting();
+      if (input === '2') return this.quitGame();
+      return this.throwError();
+    });
+  }
+
+  quitGame() {
+    Console.close();
+  }
+
+  throwError() {
+    throw new Error('[ERROR] 잘못된 값을 입력하셨습니다. 게임이 종료됩니다.');
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/MakeRandomNumbers.js
+++
@@ -1,7 +0,0 @@
-const { Random } = require("@woowacourse/mission-utils");
-
-const MakeRandomNumber = () => {
-  return Random.pickUniqueNumbersInRange(1, 9, 3);
-};
-
-module.exports = MakeRandomNumber;
